0a492a5c335493fdab971195edef87d3f1637551
Utkarsh Choudhary <72205579+UtkarshChoudhary19@users.noreply.github.com>
2023-06-15 18:36:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 //your code here
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-
+let articles = ["a", "an" , "the"];
+touristSpots.split('');
+if(touristSpots == articles){
+	touristSpots.remove();
+}
+touristSpots.join('');
 touristSpots.sort();
 
